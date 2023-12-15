27c8b33b883f41a6a53f11bd6f3ce5a257518a3b
Silvia Bacova <132918438+sivliabacova@users.noreply.github.com>
2023-12-15 17:32:37 +0100

    Update PublicFacade.php

--- PublicFacade.php
+++ PublicFacade.php
@@ -11,6 +11,7 @@ class BaseController extends AbstractController
             &knemlčkhnwjklehwe
             gekljnwjhjwnjwh
             ôknmlekhnelnghkwnelkhmn
+            poewhjwehjpwe
             hlkwehlewhnknlkhwek
             hewhmklewkhwekl
             gepúkhowrpkheopkw
